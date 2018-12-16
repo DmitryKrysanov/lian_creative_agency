6a51f7f5f2282a264d57039c8bc7c92918c31e60
Dima <dim.kry.lis@gmail.com>
2018-12-16 16:20:05 +0200

    added carousel buttons

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
         loop:true,
         margin:0,
         nav:true,
-        navText: [' ', ' '],
+        navText: ['<i class="icon-prev_arrow"></i>', '<i class="icon-next_arrow"></i>'],
+        // navText: [
+        //     '<span aria-label="' + 'Previous' + '">&#x2039;</span>',
+        //     '<span aria-label="' + 'Next' + '">&#x203a;</span>'
+        // ],
 
         responsive:{
             0:{
